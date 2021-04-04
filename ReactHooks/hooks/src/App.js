639c1a7a9ea72acd53d1fac00ac4dd419b1bcc46
Junting <342766475@qq.com>
2021-04-04 17:11:59 +0800

    ReactHooks | 自定义 hook

--- ReactHooks/hooks/src/App.js
+++ ReactHooks/hooks/src/App.js
@@ -1,13 +1,37 @@
-import React, { useRef } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import './App.css';
 
+const useGithubUsers = () => {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    axios.get('https://api.github.com/users').then(res => {
+      const { data, status } = res
+
+      if (status === 200) {
+        setUsers(data)
+        console.log("🚀 ~ file: App.js ~ line 15 ~ axios.get ~ data", data)
+      }
+    })
+  }, [])
+  return [users, setUsers]
+}
+
 function App() {
-  const boxElement = useRef()
+  const [ users, setUsers ] = useGithubUsers()
 
   return (
-    <div ref={boxElement} className="App">
-      <button onClick={() => console.log(boxElement) }>获取 DIV</button>
+    <div className="App">
+      {
+        users.map(user => (
+          <p key={user.login}>
+            <span>{user.login}</span> =>
+            <span>{user.html_url}</span>
+          </p>
+        ))
+      }
     </div>
   );
 }
